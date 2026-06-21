4d14d88ae6364b645a7a965ca8ce58e810b69e9f
Lukas Lehmann <lukaslehmann05@gmail.com>
2026-06-21 11:11:22 +0200

    added markers to title and selectors, restructured code, added controller to index

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,130 @@
 import './styles/style.scss'
+
+declare global {
+    interface Window {
+        selectTheme: typeof selectTheme
+        selectPlayer: typeof selectPlayer
+        selectBoardSize: typeof selectBoardSize
+    }
+}
+
+let selectedTheme: string = "code-vibe"
+let selectedPlayer: string = "blue"
+let selectedSize: number = 16
+let fieldSizeRaw = "s"
+
+import { returnMarker } from './script/template'
+
+const allThemes: string[] = ["code-vibe", "food", "DA-Project", "gaming"]
+const allPlayer: string[] = ["blue", "orange"]
+const allSize: string[] = ["s", "m", "l"]
+
+function selectTheme(theme: string) {
+    if (allThemes.includes(theme)) {
+        removeMarker("theme", selectedTheme)
+        selectedTheme = theme
+        displaySelected(theme, "theme")
+        addMarker("theme", theme)
+    }
+}
+
+function selectPlayer(player: string) {
+    if (allPlayer.includes(player)) {
+        removeMarker("player", selectedPlayer)
+        selectedPlayer = player
+        displaySelected(player, "player")
+        addMarker("player", player)
+    }
+}
+
+function selectBoardSize(amount: string) {
+    switch (amount) {
+        case "s":
+            setBoardSizeVisual("s", 16)
+            break;
+        case "m":
+            setBoardSizeVisual("m", 24)
+            break;
+        case "l":
+            setBoardSizeVisual("l", 32)
+            break;
+
+        default:
+            setBoardSizeVisual("s", 16)
+            break;
+    }
+}
+
+function setBoardSizeVisual(sizeRaw: string, size: number) {
+    removeMarker("size", fieldSizeRaw)
+    selectedSize = 16
+    fieldSizeRaw = sizeRaw
+    displaySelected(sizeRaw, "size")
+    addMarker("size", fieldSizeRaw)
+}
+
+function displaySelected(id: string, group: string) {
+    if (group == "player") {
+        removeOldPlayer(id)
+    } else if (group == "size") {
+        removeOldSize(id)
+    } else if (group == "theme") {
+        removeOldTheme(id)
+    }
+    document.getElementById(id)?.classList.add("selected")
+}
+
+function removeOldPlayer(id: string) {
+    allPlayer.forEach(player => {
+        if (id !== player) {
+            let elementToCheck = document.getElementById(player)
+            if (elementToCheck?.classList.contains("selected")) {
+                elementToCheck.classList.remove("selected")
+            }
+        }
+    });
+}
+
+function removeOldTheme(id: string) {
+    allThemes.forEach(theme => {
+        if (id !== theme) {
+            let elementToCheck = document.getElementById(theme)
+            if (elementToCheck?.classList.contains("selected")) {
+                elementToCheck.classList.remove("selected")
+            }
+        }
+    });
+}
+
+function removeOldSize(id: string) {
+    allSize.forEach(size => {
+        if (id !== size) {
+            let elementToCheck = document.getElementById(size)
+            if (elementToCheck?.classList.contains("selected")) {
+                elementToCheck.classList.remove("selected")
+            }
+        }
+    });
+}
+
+function addMarker(group: string, item: string) {
+    let generatedID = group + "_" + item
+    let template = returnMarker(generatedID)
+    document.getElementById(item)!.innerHTML += template
+}
+
+function removeMarker(group: string, item: string) {
+    document.getElementById(group + "_" + item)?.remove()
+}
+
+function initMarker() {
+    addMarker("theme", selectedTheme)
+    addMarker("player", selectedPlayer)
+    addMarker("size", fieldSizeRaw)
+}
+
+window.selectTheme = selectTheme
+window.selectPlayer = selectPlayer
+window.selectBoardSize = selectBoardSize
+
+window.addEventListener("load",initMarker)
---
+++ src/script/template.ts
@@ -0,0 +1,3 @@
+export function returnMarker(id:string) {
+    return `<div class="underline-container rotate-180 " id="${id}"><div class="underline-block"></div><div class="underline-line-short"></div></div>`
+}
